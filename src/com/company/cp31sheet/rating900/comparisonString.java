be8c7423849f723915738baea76d445b4c8db651
shameertaggar <shameertaggar@gmail.com>
2025-03-21 01:44:44 +0530

    Added new question

--- src/com/company/cp31sheet/rating900/comparisonString.java
+++ src/com/company/cp31sheet/rating900/comparisonString.java
@@ -1,7 +1,23 @@
 package com.company.cp31sheet.rating900;
 
+import java.util.Scanner;
+
 public class comparisonString {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int n = sc.nextInt();
+            sc.nextLine();
+            String s = sc.nextLine();
+            int cost = 1;
+            int ans = 1;
+            for (int i = 1; i<n; i++){
+                if (s.charAt(i) == s.charAt(i-1)) cost++;
+                else cost = 1;
+                ans = Math.max(Math.abs(cost),ans);
+            }
+            System.out.println(ans+1);
+        }
     }
 }
---
+++ src/com/company/cp31sheet/rating900/permutationSwap.java
@@ -0,0 +1,4 @@
+package com.company.cp31sheet.rating900;
+
+public class permutationSwap {
+}
